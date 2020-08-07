eb27acfa53dbe963bdc1df95b7f2b1f0cede6776
AlinaWillow <68222855+AlinaWillow@users.noreply.github.com>
2020-08-07 16:11:09 +0700

    update

--- fish_feeder_WIFI/fish_feeder_WIFI.ino
+++ fish_feeder_WIFI/fish_feeder_WIFI.ino
@@ -1,6 +1,6 @@
+#include <ButtonDebounce.h>
+
 #define BLYNK_PRINT Serial
-
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Servo.h>
@@ -15,19 +15,20 @@ char ssid[] = "PTNP";
 char pass[] = "Wiboonbajaree1969";
 Servo servo; //D6
 int sw = D1;
+ButtonDebounce button(D1, 250);
 
 BLYNK_WRITE(V3)
 {
   if (param.asInt()==0){
-    for(int i=0;i<=1;i+=1)
-    {
-        servo.write(45);
-        delay(500);
-        servo.write(0); 
-        delay(500);    
-    }
+ 
+        servo.write(0);
+        Serial.print("0 tele "); 
+        delay(500); 
+        servo.write(15);  
+        delay(500); 
+        Serial.println("15 tele");
+ 
   }
-  else{servo.write(0);}
 
 }
 
@@ -43,17 +44,24 @@ void setup()
 
   servo.attach(12);
   pinMode(sw, INPUT_PULLUP);
+  button.setCallback(buttonChanged);
   
 }
 
 void loop()
 {
   Blynk.run();
+  button.update();
   
+}
+void buttonChanged(int state){
+  Serial.println("Changed: " + String(state));
   if(digitalRead(sw)== 0){
-    servo.write(45);
+    servo.write(15);
+    Serial.println("15 Botton");
   }
   else{
     servo.write(0);
+    Serial.println("0 Botton");
   }
 }
